1e0bde58d463eefdacddf8ed3b14220a44474fc0
Jun Wang <junwang2003@gmail.com>
2020-02-24 22:08:24 -0600

    Add Redis Example code

---
+++ RedisConsoleExample/Program.cs
@@ -0,0 +1,73 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Configuration;
+using System.IO;
+
+namespace RedisConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Connection refers to a property that returns a ConnectionMultiplexer
+            // as shown in the previous example.
+            IDatabase cache = lazyConnection.Value.GetDatabase();
+
+            // Perform cache operations using the cache object...
+
+            // Simple PING command
+            string cacheCommand = "PING";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : " + cache.Execute(cacheCommand).ToString());
+
+            // Simple get and put of integral data types into the cache
+            cacheCommand = "GET Message";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Message").ToString());
+
+            cacheCommand = "SET Message \"Hello! The cache is working from a .NET console app!\"";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringSet()");
+            Console.WriteLine("Cache response : " + cache.StringSet("Message", "Hello! The cache is working from a .NET console app!").ToString());
+
+            // Demonstrate "SET Message" executed as expected...
+            cacheCommand = "GET Message";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Message").ToString());
+
+            // Get the client list, useful to see if connection list is growing...
+            cacheCommand = "CLIENT LIST";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : \n" + cache.Execute("CLIENT", "LIST").ToString().Replace("id=", "id="));
+
+            // Set specific screen template into cache....
+            string template = System.IO.File.ReadAllText("./template/screen_demo.json");
+            cacheCommand = $"SET Client1_Screen_Template_V2162 {template}";
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : \n" + cache.StringSet("Client1_Screen_Template_V2162", template));
+
+            cacheCommand = "GET Client1_Screen_Template_V2162";
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Client1_Screen_Template_V2162").ToString());
+
+            File.WriteAllText("./template/screen_demo_out.json", cache.StringGet("Client1_Screen_Template_V2162").ToString());
+
+            lazyConnection.Value.Dispose();
+        }
+
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            string cacheConnection = ConfigurationManager.AppSettings["CacheConnection"].ToString();
+            return ConnectionMultiplexer.Connect(cacheConnection);
+        });
+
+        public static ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return lazyConnection.Value;
+            }
+        }
+    }
+
+
+}
